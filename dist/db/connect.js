1bac3e83eefeecbcb83b8b58d9ff2dd6648e3808
d1ofmany <nikola.barkovic@gmail.com>
2017-07-18 09:27:13 +0000

    Mongo connection modification for Heroku

--- dist/db/connect.js
+++ dist/db/connect.js
@@ -1,20 +1,27 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 
 var _mongoose = require('mongoose');
 
 var _mongoose2 = _interopRequireDefault(_mongoose);
 
-var _config = require('../../config.json');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _mongoose2.default.Promise = Promise;
 
-_mongoose2.default.connect('mongodb://' + _config.mongo.username + ':' + _config.mongo.password + '@' + _config.mongo.db);
+var connectDB = 'mongodb://localhost/wfl';
+
+if (process.env.MONGO_URI) {
+    connectDB = process.env.MONGO_URI;
+} else {
+    var config = require('../../config.json');
+    connectDB = 'mongodb://' + config.mongo.username + ':' + config.mongo.password + '@' + config.mongo.db;
+}
+
+_mongoose2.default.connect(connectDB);
 
 exports.default = _mongoose2.default;
 //# sourceMappingURL=connect.js.map
--- src/db/connect.js
+++ src/db/connect.js
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose';
-import { mongo } from '../../config.json';
 
 mongoose.Promise = Promise;
 
-mongoose.connect(`mongodb://${mongo.username}:${mongo.password}@${mongo.db}`);
+let connectDB = 'mongodb://localhost/wfl'
+
+if (process.env.MONGO_URI) {
+    connectDB = process.env.MONGO_URI;
+} else {
+    const config = require('../../config.json');
+    connectDB = `mongodb://${config.mongo.username}:${config.mongo.password}@${config.mongo.db}`;
+}
+
+mongoose.connect(connectDB);
 
 export default mongoose;
